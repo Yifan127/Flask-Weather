f860b1fa9b747eeef51fd242d583e998d0c79a39
Yifan Zhang <yifan127@gmail.com>
2017-02-22 21:15:53 +1300

    bind port

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
 from weather import app
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host='0.0.0.0', debug=False)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
