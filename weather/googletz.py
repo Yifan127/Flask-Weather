eb0fc2bff8150898f892d24e45ac7ddfb89bb398
Yifan Zhang <yifan127@gmail.com>
2017-02-22 19:55:29 +1300

    remove chinese characters

--- weather/googletz.py
+++ weather/googletz.py
@@ -13,7 +13,8 @@ class TZ():
         '''get current weather data from open weather map API,
         return the response data'''
         error = None
-        params = {'location': location, 'timestamp': timestamp, 'language': self.language, 'key': self.key}
+        params = {'location': location, 'timestamp': timestamp,
+                  'language': self.language, 'key': self.key}
         try:
             r = requests.get(self.api, params=params, timeout=self.timeout)
             if r.status_code == requests.codes.ok:
@@ -23,8 +24,8 @@ class TZ():
                 location_timestamp = timestamp + dstOffset + rawOffset
                 return location_timestamp
             else:
-                error = '访问Google Time Zone API失败!'
+                error = 'Fail to access Google Time Zone API!'
                 return error
         except (requests.Timeout, requests.ConnectionError):
-            error = '网络异常或者OpenWeatherMap API连接超时!'
+            error = 'Network problem or OpenWeatherMap API connection timeout!'
             return error
